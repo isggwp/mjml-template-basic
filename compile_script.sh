f6e1b6dc0cec70f682b9a980aaa645a023a96c07
isggwp <indrabinridwan@gmail.com>
2022-10-28 09:03:33 +0700

    mjml email template

---
+++ compile_script.sh
@@ -0,0 +1,11 @@
+mjml after-create-account.mjml -o compiled/after-create-account.html
+mjml after-successfull-purchase.mjml -o compiled/after-successfull-purchase.html
+mjml Duedate-notification-pva.mjml -o compiled/Duedate-notification-pva.html
+mjml konfirmasi-pencairan-poin.mjml -o compiled/konfirmasi-pencairan-poin.html
+mjml konfirmasi-poin-telah-dicairkan.mjml -o compiled/konfirmasi-poin-telah-dicairkan.html
+mjml otp-email.mjml -o compiled/otp-email.html
+mjml payment-failure-cc.mjml -o compiled/payment-failure-cc.html
+mjml payment-success-cc.mjml -o compiled/payment-success-cc.html
+mjml reset-password.mjml -o compiled/reset-password.html
+mjml submit-klaim-asuransi.mjml -o compiled/submit-klaim-asuransi.html
+
